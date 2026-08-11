7fa683cd2d4fec4723e26d751b7ea4662c6efc02
Deepanshu <ds484863@gmail.com>
2026-08-11 18:03:01 +0530

    Time: 2 ms (59.75%) | Memory: 43.1 MB (39.61%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,31 +1,29 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-
-        Stack<Character> st1 = new Stack<>();
-        Stack<Character> st2 = new Stack<>();
-
-        // Process s
-        for (char ch : s.toCharArray()) {
-            if (ch == '#') {
-                if (!st1.isEmpty()) {
-                    st1.pop();
-                }
-            } else {
-                st1.push(ch);
-            }
+   
+   Stack<Character> st1 = new Stack<>();
+   Stack<Character> st2 = new Stack<>();
+   for(char ch : s.toCharArray()){
+    if(ch == '#'){
+        if(!st1.isEmpty()){
+            st1.pop();
         }
-
-        // Process t
-        for (char ch : t.toCharArray()) {
-            if (ch == '#') {
-                if (!st2.isEmpty()) {
+    }
+        else{
+            st1.push(ch);
+        }
+        }
+        for(char ch : t.toCharArray()){
+            if(ch == '#'){
+                if(!st2.isEmpty()){
                     st2.pop();
                 }
-            } else {
+            }
+            else{
                 st2.push(ch);
             }
         }
-
         return st1.equals(st2);
-    }
-}
+   
+}   
+} 
