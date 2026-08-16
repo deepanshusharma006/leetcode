ef00c0694cb1475349f2c6848ee026a158119122
Deepanshu <ds484863@gmail.com>
2026-08-16 14:34:45 +0530

    Time: 4 ms (75.68%) | Memory: 115 MB (16.22%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int[] count = new int[3];
+
+        // Count remainders
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+
+        // If number of 0-remainder stones is even
+        if (count[0] % 2 == 0) {
+            return count[1] > 0 && count[2] > 0;
+        }
+
+        // If number of 0-remainder stones is odd
+        return Math.abs(count[1] - count[2]) > 2;
+    }
+}
