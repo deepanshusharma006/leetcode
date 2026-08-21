95c40a0e36542576be8cdfa18b7eb21285106c77
Deepanshu <ds484863@gmail.com>
2026-08-21 21:28:34 +0530

    Time: 170 ms (19.61%) | Memory: 43.5 MB (78.43%) - LeetSync

---
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
@@ -0,0 +1,89 @@
+class Solution {
+
+    public long findKthSmallest(int[] coins, int k) {
+
+        long left = 1;
+        long right = (long) k * minCoin(coins);
+
+        while (left < right) {
+
+            long mid = left + (right - left) / 2;
+
+            if (count(mid, coins) >= k) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private long count(long x, int[] coins) {
+
+        long ans = 0;
+        int n = coins.length;
+
+        // All subsets
+        for (int mask = 1; mask < (1 << n); mask++) {
+
+            long lcm = 1;
+            int bits = 0;
+
+            for (int i = 0; i < n; i++) {
+
+                if ((mask & (1 << i)) != 0) {
+
+                    bits++;
+
+                    lcm = lcm(lcm, coins[i]);
+
+                    if (lcm > x) {
+                        break;
+                    }
+                }
+            }
+
+            if (lcm > x) {
+                continue;
+            }
+
+            long contribution = x / lcm;
+
+            if (bits % 2 == 1) {
+                ans += contribution;
+            } else {
+                ans -= contribution;
+            }
+        }
+
+        return ans;
+    }
+
+    private long gcd(long a, long b) {
+
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+
+        return a;
+    }
+
+    private long lcm(long a, long b) {
+
+        return a / gcd(a, b) * b;
+    }
+
+    private int minCoin(int[] coins) {
+
+        int min = coins[0];
+
+        for (int coin : coins) {
+            min = Math.min(min, coin);
+        }
+
+        return min;
+    }
+}
